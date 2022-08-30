10bf751bfed8bb6537aa0e07058e43ed3465e389
ajread4 <read.austin@gmail.com>
2022-08-29 20:35:55 -0400

    working on signal flag

---
+++ phonenumber.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/python
+# Create Phone Number (6kyu)
+# Language: Python
+# By: AJ Read
+
+def create_phone_number(n):
+	return "(" + str(n[0]) + str(n[1]) + str(n[2]) + ") "+ str(n[3]) + str(n[4]) + str(n[5])+ "-"+str(n[6])+str(n[7])+str(n[8])+str(n[9s])
+
+
+print(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])) 
---
+++ smokesignal.py
@@ -0,0 +1,19 @@
+# https://www.codewars.com/kata/62a3855fcaec090025ed2a9a/train/python
+# Smoke Signal (5kyu)
+# Language: Python
+# By: AJ Read
+
+import collections
+
+def decode_smoke_signals(days):
+	final_dict=[]
+	for num,action in days: 
+
+	return final_dict
+
+print(decode_smoke_signals(
+            [(["8.2.1","4.3.4","1"],["Ambush in the jungle","General assassinated","Ambush in the jungle"]),
+            (["1","2.2","9.3"],["Ambush in the jungle","Orange army retreats","Push into the mountains"]),
+            (["4.3.4","6"],["Ambush in the jungle","Orange general goes on vacation"]),
+            (["8.2.1","9.3","1"],["Ambush in the jungle","General assassinated","Push into the mountains"])]
+        ))
